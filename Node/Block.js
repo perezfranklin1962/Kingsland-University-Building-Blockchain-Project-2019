6b971d57e1b81860b399c2cf6cdefead04ee2c03
perezfranklin1962 <57190643+perezfranklin1962@users.noreply.github.com>
2019-11-01 01:33:15 -0400

    Added contsructor arguments

--- Node/Block.js
+++ Node/Block.js
@@ -1,3 +1,17 @@
 module.exports = class Block {
 
+	constructor(
+		index, // Index: integer (unsigned)
+		transactions, // Transactions : Transaction[]
+		difficulty, // Difficulty: integer (unsigned)
+		prevBlockHash, // PrevBlockHash: hex_number[64]
+		minedBy, // MinedBy: address (40 hex digits)
+
+		// Below will be SHA256 of the above
+		blockDataHash, // BlockDataHash: hex_number[64]
+
+		// Assigned by the Miners
+		nonce, // Nonce: integer (unsigned)
+		dateCreated, // DateCreated : ISO8601_string
+		blockHash // BlockHash: hex_number[64]
 };
